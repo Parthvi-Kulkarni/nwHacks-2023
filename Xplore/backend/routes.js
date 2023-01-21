a948b2a81197244b4736e50fc09b5a6ba3407385
Parthvi Kulkarni <parthvikulkarni@dhcp-128-189-93-240.ubcsecure.wireless.ubc.ca>
2023-01-21 13:50:30 -0800

    removed updateUser

--- Xplore/backend/routes.js
+++ Xplore/backend/routes.js
@@ -1,13 +1,12 @@
 const Router = require('express-promise-router');
 
-const { getUser, createUser, updateUser } = require('./controllers/userController');
+const { getUser, createUser} = require('./controllers/userController');
 
 const router = Router();
 
 // user
 router.get('/user/:id', getUser);
 router.post('/user', createUser);
-router.put('/user', updateUser);
 
 
 module.exports = router;
